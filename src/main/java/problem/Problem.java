361bb5c2e663b3bac252295a68c49c9a9c272dc9
IGOREK3000 <79245743+IGOREK3000@users.noreply.github.com>
2021-04-24 22:40:53 +0300

    Update Problem.java

--- src/main/java/problem/Problem.java
+++ src/main/java/problem/Problem.java
@@ -101,7 +101,7 @@ public class Problem {
                     Vector2[] points = new Vector2[]{p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24};
 
 
-                    Vector2 polygon[] = new Vector2[]{null, null, null, null, null, null, null};
+                    Vector2 polygon[] = new Vector2[]{null, null, null, null, null, null, null, null};
                     int l1 = 0;
                     for (int i = 0; i < 24; i++) {
                         System.out.println(points[i]);
